c60bf9fde70124e9d58cb11656d6c9e5859ea0ca
ignacio-montaldi <ignaciomontaldi@gmail.com>
2024-10-08 12:30:13 -0300

    Solucionado error en el formato de fecha

--- app/src/utils/dateFormatter.ts
+++ app/src/utils/dateFormatter.ts
@@ -1,7 +1,7 @@
 export function longDateFormatter() : string {
 
     const date = new Date(Date.now());
-    const formatter = {weekday: 'long', day: 'numeric', month: 'long'}
+    const formatter:Intl.DateTimeFormatOptions = {weekday: 'long', day: 'numeric', month: 'long'}
     let dateFormatted = date.toLocaleDateString('es-ES', formatter);
     dateFormatted = dateFormatted.charAt(0).toUpperCase() + dateFormatted.slice(1);
     return dateFormatted;
